c9d673515c4a0f0a63291ae58f784c78d1609cde
Anastasia <112168910+Anastasiamay@users.noreply.github.com>
2023-04-09 13:46:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,10 @@ import pandas as pd
 import numpy as np
 from scipy.stats import ttest_1samp
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1067114867 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool:
-    t_statistic, p_value = ttest_1samp(x, 300)
-    
-    if (p_value < 0.06).all():
-        return True  
-    else:
-        return False
+    alpha = 0.06
+    t_stat, p_value = ttest_1samp(x, 300)
+    return p_value/2 < alpha and t_stat < 0
     
