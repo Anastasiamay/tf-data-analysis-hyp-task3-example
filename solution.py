83a497a2a8839bce558389a91e58d1dfbf5a7bee
Anastasia <112168910+Anastasiamay@users.noreply.github.com>
2023-04-09 13:11:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import ttest_1samp
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array) -> bool:
+    t_statistic, p_value = ttest_1samp(x, 300)
+    
+    if (p_value < 0.06).all():
+        return True  
+    else:
+        return False
+    
